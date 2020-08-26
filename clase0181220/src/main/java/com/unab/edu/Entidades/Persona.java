6233468a15504e6e763da8970c094c8d56a56675
Dayana Ivette <63428525+dayanaivette@users.noreply.github.com>
2020-08-25 19:38:07 -0600

    Clase13082020
     Primer Avance

---
+++ clase0181220/src/main/java/com/unab/edu/Entidades/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.Entidades;
+
+import lombok.Data;
+
+/**
+ *
+ * @author dayan
+ */
+
+@Data
+public class Persona {
+    
+    int idPersona;
+    String Nombre;
+    String Apellido;
+    int Edad;
+    String Sexo;
+    
+}
